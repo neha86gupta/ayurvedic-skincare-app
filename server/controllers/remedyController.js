e61400e1e6d796c3d6b2225e1ee46308525450b3
neha86gupta <56247518+neha86gupta@users.noreply.github.com>
2026-03-09 23:25:57 +0530

    Adding remedy recommendations functionality.

---
+++ server/controllers/remedyController.js
@@ -0,0 +1,10 @@
+// Remedy Controller
+
+class RemedyController {
+    static getRemedies(req, res) {
+        // Logic to get remedies based on user input
+        res.json({ message: 'Getting remedies ...' });
+    }
+}
+
+module.exports = RemedyController;
